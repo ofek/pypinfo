1c77bb000546c640325e4a08ade3e8517b5e2099
ofek <ofekmeister@gmail.com>
2017-05-13 16:30:02 -0400

    init

---
+++ README.rst
@@ -0,0 +1,21 @@
+pypinfo: View PyPI download statistics with ease.
+=================================================
+
+.. image:: https://img.shields.io/pypi/v/pypinfo.svg?style=flat-square
+    :target: https://pypi.org/project/pypinfo
+
+.. image:: https://img.shields.io/travis/ofek/pypinfo.svg?branch=master&style=flat-square
+    :target: https://travis-ci.org/ofek/pypinfo
+
+.. image:: https://img.shields.io/codecov/c/github/ofek/pypinfo.svg?style=flat-square
+    :target: https://codecov.io/gh/ofek/pypinfo
+
+.. image:: https://img.shields.io/pypi/pyversions/pypinfo.svg?style=flat-square
+    :target: https://pypi.org/project/pypinfo
+
+.. image:: https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square
+    :target: https://en.wikipedia.org/wiki/MIT_License
+
+-----
+
+pypinfo is a simple CLI to access PyPI download statistics via Google's BigQuery.
---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import find_packages, setup
+
+with open('pypinfo/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('= ')[1].strip("'")
+            break
+
+setup(
+    name='pypinfo',
+    version=version,
+    description='Bitcoin made easy.',
+    long_description=open('README.rst', 'r').read(),
+    author='Ofek Lev',
+    author_email='ofekmeister@gmail.com',
+    maintainer='Ofek Lev',
+    maintainer_email='ofekmeister@gmail.com',
+    url='https://github.com/ofek/pypinfo',
+    download_url='https://github.com/ofek/pypinfo',
+    license='MIT',
+
+    keywords=(
+        'pypi',
+        'downloads',
+        'stats',
+        'bigquery',
+    ),
+
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ),
+
+    install_requires=('click', 'google-cloud-bigquery'),
+    tests_require=['pytest'],
+
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': (
+            'pypinfo = pypinfo.cli:pypinfo',
+        ),
+    },
+)
