49c938285337bd5b8c48f21b90aced22c0b0d39c
Hugo <hugovk@users.noreply.github.com>
2017-10-17 12:46:03 +0300

    Fix static analysis

--- pypinfo/fields.py
+++ pypinfo/fields.py
@@ -8,18 +8,26 @@ Year = Field('download_year', 'STRFTIME_UTC_USEC(timestamp, "%Y")')
 Country = Field('country', 'country_code')
 Project = Field('project', 'file.project')
 Version = Field('version', 'file.version')
-PythonVersion = Field('python_version', 'REGEXP_EXTRACT(details.python, r"^([^\.]+\.[^\.]+)")')
+PythonVersion = Field(
+    'python_version',
+    'REGEXP_EXTRACT(details.python, r"^([^\.]+\.[^\.]+)")')
 Percent3 = Field(
     'percent_3',
-    'ROUND(100 * SUM(CASE WHEN REGEXP_EXTRACT(details.python, r"^([^\.]+)") = "3" THEN 1 ELSE 0 END) / COUNT(*), 1)'
+    'ROUND(100 * SUM(CASE WHEN REGEXP_EXTRACT(details.python, r"^([^\.]+)") = '
+    '"3" THEN 1 ELSE 0 END) / COUNT(*), 1)'
 )
 Percent2 = Field(
     'percent_2',
-    'ROUND(100 * SUM(CASE WHEN REGEXP_EXTRACT(details.python, r"^([^\.]+)") = "2" THEN 1 ELSE 0 END) / COUNT(*), 1)'
+    'ROUND(100 * SUM(CASE WHEN REGEXP_EXTRACT(details.python, r"^([^\.]+)") = '
+    '"2" THEN 1 ELSE 0 END) / COUNT(*), 1)'
 )
 Implementation = Field('implementation', 'details.implementation.name')
-ImplementationVersion = Field('impl_version', 'REGEXP_EXTRACT(details.implementation.version, r"^([^\.]+\.[^\.]+)")')
-OpenSSLVersion = Field('openssl_version', 'REGEXP_EXTRACT(details.openssl_version, r"^OpenSSL ([^ ]+) ")')
+ImplementationVersion = Field(
+    'impl_version',
+    'REGEXP_EXTRACT(details.implementation.version, r"^([^\.]+\.[^\.]+)")')
+OpenSSLVersion = Field(
+    'openssl_version',
+    'REGEXP_EXTRACT(details.openssl_version, r"^OpenSSL ([^ ]+) ")')
 Installer = Field('installer_name', 'details.installer.name')
 InstallerVersion = Field('installer_version', 'details.installer.version')
 SetuptoolsVersion = Field('setuptools_version', 'details.setuptools_version')
