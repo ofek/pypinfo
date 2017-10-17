ecfe9f871e33194ad098e56bc93e33c32d5ecf06
Hugo <hugovk@users.noreply.github.com>
2017-10-17 17:24:46 +0300

    Add 'Python :: 3 :: Only' classifier (#21)

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ),
