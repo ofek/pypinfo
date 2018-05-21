755bfcb4d069c40c66ba19df6e2cefe712980e58
Hugo <hugovk@users.noreply.github.com>
2018-05-21 17:36:25 +0300

    Add --all option, default to only pip downloads (#51)
    * Add --all option, default to --pip
    
    * Ignore some test files
    
    * Remove --pip option

--- pypinfo/cli.py
+++ pypinfo/cli.py
@@ -61,13 +61,15 @@ TO_CENTS = Decimal('0.00')
 @click.option('--end-date', '-ed', help='Must be negative. Default: -1')
 @click.option('--where', '-w', help='WHERE conditional. Default: file.project = "project"')
 @click.option('--order', '-o', help='Field to order by. Default: download_count')
-@click.option('--pip', '-p', is_flag=True, help='Only show installs by pip.')
+@click.option('--all', 'all_installers', is_flag=True,
+              help='Show downloads by all installers, not only pip.')
 @click.option('--percent', '-pc', is_flag=True, help='Print percentages.')
 @click.option('--markdown', '-md', is_flag=True, help='Output as Markdown.')
 @click.version_option()
 @click.pass_context
 def pypinfo(ctx, project, fields, auth, run, json, indent, timeout, limit, days,
-            start_date, end_date, where, order, pip, percent, markdown):
+            start_date, end_date, where, order, all_installers, percent,
+            markdown):
     """Valid fields are:\n
     project | version | file | pyversion | percent3 | percent2 | impl | impl-version |\n
     openssl | date | month | year | country | installer | installer-version |\n
@@ -97,7 +99,7 @@ def pypinfo(ctx, project, fields, auth, run, json, indent, timeout, limit, days,
 
     built_query = build_query(
         project, parsed_fields, limit=limit, days=days, start_date=start_date,
-        end_date=end_date, where=where, order=order_name, pip=pip
+        end_date=end_date, where=where, order=order_name, pip=not all_installers,
     )
 
     if run:
